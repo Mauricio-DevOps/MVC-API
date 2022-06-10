35fe88ecd67bfbee0371cf87eae4f81bf40d774e
Mauricio-DevOps <mnmarquesgames@gmail.com>
2022-06-10 02:53:58 -0300

    Mauricio
    Ajuste, Projeto

--- CrudImovel/Imovel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Imovel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
